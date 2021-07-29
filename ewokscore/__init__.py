18161b1a79a280c51b5b4a8ebeb516838dec4256
woutdenolf <woutdenolf@users.sf.net>
2021-07-29 10:46:20 +0200

    bump version

--- ewokscore/__init__.py
+++ ewokscore/__init__.py
@@ -2,4 +2,4 @@ from .task import Task  # noqa: F401
 from .graph import load_graph  # noqa: F401
 from .graph import execute_graph  # noqa: F401
 
-__version__ = "0.0.1-alpha"
+__version__ = "0.0.1-alpha.1"
