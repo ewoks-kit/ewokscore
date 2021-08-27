e32651b1d69418c0390653ff0862e23d667d6614
woutdenolf <woutdenolf@users.sf.net>
2021-08-27 17:58:23 +0200

    use the new "sub_graph_nodes" link attribute in tests

--- ewokscore/tests/test_sub_graph.py
+++ ewokscore/tests/test_sub_graph.py
@@ -50,7 +50,8 @@ def subsubgraph(tmpdir, subsubsubgraph):
             {
                 "source": "task2",
                 "target": "subsubsubgraph",
-                "links": [{"target": "task1", "arguments": {0: "return_value"}}],
+                "arguments": {0: "return_value"},
+                "sub_graph_nodes": {"sub_target": "task1"},
             },
         ],
     }
@@ -76,7 +77,8 @@ def subgraph(tmpdir, subsubgraph):
             {
                 "source": "task2",
                 "target": "subsubgraph",
-                "links": [{"target": "task1", "arguments": {0: "return_value"}}],
+                "arguments": {0: "return_value"},
+                "sub_graph_nodes": {"sub_target": "task1"},
             },
         ],
     }
@@ -98,77 +100,113 @@ def graph(tmpdir, subgraph):
             {
                 "source": "subgraph1",
                 "target": "subgraph2",
-                "links": [
-                    {
-                        "source": "subsubgraph",
-                        "target": "task1",
-                        "links": [
-                            {
-                                "source": "subsubsubgraph",
-                                "links": [
-                                    {
-                                        "source": "task2",
-                                        "arguments": {0: "return_value"},
-                                    }
-                                ],
-                            },
-                        ],
+                "arguments": {0: "return_value"},
+                "sub_graph_nodes": {
+                    "sub_source": "subsubgraph",
+                    "sub_target": "task1",
+                    "sub_graph_nodes": {
+                        "sub_source": "subsubsubgraph",
+                        "sub_graph_nodes": {"sub_source": "task2"},
                     },
-                ],
+                },
             },
             # Expanded sub-links
             {
                 "source": "subgraph1",
                 "target": "append",
-                "links": [
-                    {"source": "task1", "arguments": {0: "return_value"}},
-                    {"source": "task2", "arguments": {1: "return_value"}},
-                    {
-                        "source": "subsubgraph",
-                        "links": [
-                            {"source": "task1", "arguments": {2: "return_value"}},
-                            {"source": "task2", "arguments": {3: "return_value"}},
-                            {
-                                "source": "subsubsubgraph",
-                                "links": [
-                                    {
-                                        "source": "task1",
-                                        "arguments": {4: "return_value"},
-                                    },
-                                    {
-                                        "source": "task2",
-                                        "arguments": {5: "return_value"},
-                                    },
-                                ],
-                            },
-                        ],
+                "arguments": {0: "return_value"},
+                "sub_graph_nodes": {"sub_source": "task1"},
+            },
+            {
+                "source": "subgraph1",
+                "target": "append",
+                "arguments": {1: "return_value"},
+                "sub_graph_nodes": {"sub_source": "task2"},
+            },
+            {
+                "source": "subgraph1",
+                "target": "append",
+                "arguments": {2: "return_value"},
+                "sub_graph_nodes": {
+                    "sub_source": "subsubgraph",
+                    "sub_graph_nodes": {"sub_source": "task1"},
+                },
+            },
+            {
+                "source": "subgraph1",
+                "target": "append",
+                "arguments": {3: "return_value"},
+                "sub_graph_nodes": {
+                    "sub_source": "subsubgraph",
+                    "sub_graph_nodes": {"sub_source": "task2"},
+                },
+            },
+            {
+                "source": "subgraph1",
+                "target": "append",
+                "arguments": {4: "return_value"},
+                "sub_graph_nodes": {
+                    "sub_source": "subsubgraph",
+                    "sub_graph_nodes": {
+                        "sub_source": "subsubsubgraph",
+                        "sub_graph_nodes": {"sub_source": "task1"},
                     },
-                ],
+                },
+            },
+            {
+                "source": "subgraph1",
+                "target": "append",
+                "arguments": {5: "return_value"},
+                "sub_graph_nodes": {
+                    "sub_source": "subsubgraph",
+                    "sub_graph_nodes": {
+                        "sub_source": "subsubsubgraph",
+                        "sub_graph_nodes": {"sub_source": "task2"},
+                    },
+                },
             },
             # Flat sub-links (1 level deep because the source and target need to be a valid node id)
             {
                 "source": "subgraph2",
                 "target": "append",
-                "links": [
-                    {"source": "task1", "arguments": {6: "return_value"}},
-                    {"source": "task2", "arguments": {7: "return_value"}},
-                    {
-                        "source": ("subsubgraph", "task1"),
-                        "arguments": {8: "return_value"},
-                    },
-                    {
-                        "source": ("subsubgraph", "task2"),
-                        "arguments": {9: "return_value"},
-                    },
-                    {
-                        "source": ("subsubgraph", ("subsubsubgraph", "task1")),
-                        "arguments": {10: "return_value"},
-                    },
-                    {
-                        "source": ("subsubgraph", ("subsubsubgraph", "task2")),
-                        "arguments": {11: "return_value"},
-                    },
-                ],
+                "arguments": {6: "return_value"},
+                "sub_graph_nodes": {"sub_source": "task1"},
+            },
+            {
+                "source": "subgraph2",
+                "target": "append",
+                "arguments": {7: "return_value"},
+                "sub_graph_nodes": {"sub_source": "task2"},
+            },
+            {
+                "source": "subgraph2",
+                "target": "append",
+                "arguments": {8: "return_value"},
+                "sub_graph_nodes": {
+                    "sub_source": ("subsubgraph", "task1"),
+                },
+            },
+            {
+                "source": "subgraph2",
+                "target": "append",
+                "arguments": {9: "return_value"},
+                "sub_graph_nodes": {"sub_source": ("subsubgraph", "task2")},
+            },
+            {
+                "source": "subgraph2",
+                "target": "append",
+                "arguments": {10: "return_value"},
+                "sub_graph_nodes": {
+                    "sub_source": ("subsubgraph", ("subsubsubgraph", "task1"))
+                },
+            },
+            {
+                "source": "subgraph2",
+                "target": "append",
+                "arguments": {11: "return_value"},
+                "sub_graph_nodes": {
+                    "sub_source": ("subsubgraph", ("subsubsubgraph", "task2"))
+                },
             },
         ],
     }
