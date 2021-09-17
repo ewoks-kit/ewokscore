80183cb656236f15b6ac3e59c8558773bf198505
woutdenolf <woutdenolf@users.sf.net>
2021-09-17 12:46:03 +0200

    remove deprecated links

--- ewokscore/subgraph.py
+++ ewokscore/subgraph.py
@@ -1,27 +1,11 @@
 import itertools
 from typing import Tuple, Union, Any
-import warnings
 import networkx
 from .utils import dict_merge
 from .node import flatten_node_name
 
 
 NodeIdType = Union[str, Tuple[str, Any]]  # Any is NodeIdType
-
-
-def _pop_subgraph_node_name_deprecated(subgraph_name, link_attrs, source=True):
-    if source:
-        key = "source"
-    else:
-        key = "target"
-    try:
-        subgraph_node_name = link_attrs.pop(key)
-    except KeyError:
-        raise ValueError(
-            f"The '{key}' attribute to specify a node in subgraph '{subgraph_name}' is missing"
-        ) from None
-
-    return _append_subnode_name(subgraph_name, subgraph_node_name)
 
 
 def _append_subnode_name(node_name: NodeIdType, sub_node_name: str) -> NodeIdType:
@@ -53,46 +37,6 @@ def _get_subgraph(node_name: NodeIdType, subgraphs: dict):
     raise ValueError(
         f"{subnode_name} is not a node or subgraph of subgraph {repr(subgraph_name)}",
     )
-
-
-def _pop_subgraph_node_names_deprecated(
-    source_name, target_name, link_attrs, subgraphs
-):
-    if _get_subgraph(source_name, subgraphs):
-        source = _pop_subgraph_node_name_deprecated(
-            source_name, link_attrs, source=True
-        )
-    else:
-        link_attrs.pop("source", None)
-        source = source_name
-
-    if _get_subgraph(target_name, subgraphs):
-        target = _pop_subgraph_node_name_deprecated(
-            target_name, link_attrs, source=False
-        )
-        target_attributes = link_attrs.pop("node_attributes", None)
-    else:
-        link_attrs.pop("target", None)
-        target = target_name
-        link_attrs.pop("node_attributes", None)
-        target_attributes = None
-
-    return source, target, target_attributes
-
-
-def _extract_subgraph_links_deprecated(source_name, target_name, links, subgraphs):
-    for link_attrs in links:
-        link_attrs = dict(link_attrs)
-        source, target, target_attributes = _pop_subgraph_node_names_deprecated(
-            source_name, target_name, link_attrs, subgraphs
-        )
-        sublinks = link_attrs.pop("links", None)
-        if sublinks:
-            yield from _extract_subgraph_links_deprecated(
-                source, target, sublinks, subgraphs
-            )
-        else:
-            yield source, target, link_attrs, target_attributes
 
 
 def _resolve_node_alias(
@@ -227,44 +171,26 @@ def extract_graph_nodes(graph: networkx.DiGraph, subgraphs) -> Tuple[list, dict]
             else:
                 all_link_attrs = [all_link_attrs]
             for link_attrs in all_link_attrs:
-                links = link_attrs.pop("links", None)
                 sub_graph_nodes = {
                     key: link_attrs.pop(key)
                     for key in ["sub_source", "sub_target", "sub_target_attributes"]
                     if key in link_attrs
                 }
-                if not links and not sub_graph_nodes:
+                if not sub_graph_nodes:
                     continue
-                if links and sub_graph_nodes:
-                    raise ValueError(
-                        "cannot use link attributes 'links' and 'sub_source'/'sub_target' at the same time. 'links' is a deprecated link attribute."
-                    )
-                if links:
-                    warnings.warn(
-                        "'links' is a deprecated link attribute. Use 'sub_source' and 'sub_target' instead."
-                    )
-                    itlinks = _extract_subgraph_links_deprecated(
-                        source_name, target_name, links, subgraphs
-                    )
-                    for source, target, link_attrs, target_attributes in itlinks:
-                        if target_attributes:
-                            update_attrs[target] = target_attributes
-                        edges.append((source, target, link_attrs))
-                else:
-                    source, target, target_is_graph = _get_subnode_info(
-                        source_name, target_name, sub_graph_nodes, subgraphs
-                    )
-
-                    sub_target_attributes = sub_graph_nodes.get(
-                        "sub_target_attributes", None
-                    )
-                    if sub_target_attributes:
-                        if not target_is_graph:
-                            raise ValueError(
-                                f"'{target_name}' is not a graph so 'sub_target_attributes' should not be specified"
-                            )
-                        update_attrs[target] = sub_target_attributes
-                    edges.append((source, target, link_attrs))
+                source, target, target_is_graph = _get_subnode_info(
+                    source_name, target_name, sub_graph_nodes, subgraphs
+                )
+                sub_target_attributes = sub_graph_nodes.get(
+                    "sub_target_attributes", None
+                )
+                if sub_target_attributes:
+                    if not target_is_graph:
+                        raise ValueError(
+                            f"'{target_name}' is not a graph so 'sub_target_attributes' should not be specified"
+                        )
+                    update_attrs[target] = sub_target_attributes
+                edges.append((source, target, link_attrs))
 
     _replace_aliases(graph, subgraphs, input_nodes=True)
     _replace_aliases(graph, subgraphs, input_nodes=False)
