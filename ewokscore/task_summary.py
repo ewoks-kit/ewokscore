f3b74eec4de62b090f6e77d4ab7fe8f459c1bd6c
woutdenolf <woutdenolf@users.sf.net>
2021-11-12 11:41:22 +0100

    add the category field to the task summary

--- ewokscore/task_summary.py
+++ ewokscore/task_summary.py
@@ -4,12 +4,15 @@ from .task import Task
 def generate_task_summary() -> dict:
     summary = list()
     for cls in Task.get_subclasses():
+        task_identifier = cls.class_registry_name()
+        category = task_identifier.split(".")[0]
         info = {
             "task_type": "class",
-            "task_identifier": cls.class_registry_name(),
+            "task_identifier": task_identifier,
             "required_input_names": list(cls.required_input_names()),
             "optional_input_names": list(cls.optional_input_names()),
             "output_names": list(cls.output_names()),
+            "category": category,
         }
         summary.append(info)
     return summary
