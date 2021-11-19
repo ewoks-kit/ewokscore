2270eef1471fdcaf3ad169c0f260902158808e03
woutdenolf <woutdenolf@users.sf.net>
2021-11-19 10:57:56 +0100

    move execute_graph to different module, uniform with all other ewoks projects

--- ewokscore/graph.py
+++ ewokscore/graph.py
@@ -24,18 +24,6 @@ def load_graph(source=None, representation=None, **load_options):
         return TaskGraph(source=source, representation=representation, **load_options)
 
 
-def execute_graph(graph, load_options: Optional[dict] = None, **execute_options):
-    """
-    :param graph: graph to be executed
-    :param Optional[dict] load_options: options to provide to the `load_graph` function (and as a consequence to the `TaskGraph.load` as `root_dir`)
-    :param execute_options: options to provide to the Graph.execute function as `varinfo` or `raise_on_error`
-    """
-    if load_options is None:
-        load_options = dict()
-    graph = load_graph(source=graph, **load_options)
-    return graph.execute(**execute_options)
-
-
 def set_graph_defaults(graph_as_dict):
     graph_as_dict.setdefault("directed", True)
     graph_as_dict.setdefault("nodes", list())
