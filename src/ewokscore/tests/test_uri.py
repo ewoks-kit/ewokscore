c6c5deeb4faff62fb47ef884483c6e77ca8fe649
Loic Huder <loic.huder@esrf.fr>
2024-10-14 12:10:32 +0200

    Fix Python 3.12 CI

--- src/ewokscore/tests/test_uri.py
+++ src/ewokscore/tests/test_uri.py
@@ -1,17 +1,20 @@
 import os
+import pytest
+import sys
 from pathlib import Path
 from ewokscore.persistence.uri import parse_uri
 from ewokscore.persistence.uri import path_from_uri
 from ewokscore.persistence.uri import parse_query
 
 
-def test_file_uri():
+@pytest.mark.skipif(
+    sys.version_info >= (3, 12, 5), reason="Behaviour changes in python3.12.5"
+)
+def test_relpath_file_uri_old():
     nonpath = str(Path("file.h5"))
     relpath = str(Path("relpath") / "file.h5")
-    abspath = str(Path(os.path.sep) / "abspath" / "file.h5")
     assert not os.path.isabs(nonpath)
     assert not os.path.isabs(relpath)
-    assert os.path.isabs(abspath)
 
     uri = nonpath
     assert parse_uri(uri).geturl() == "file:///file.h5"
@@ -20,11 +23,6 @@ def test_file_uri():
 
     uri = relpath
     assert parse_uri(uri).geturl() == "file:///relpath/file.h5"
-    assert str(path_from_uri(parse_uri(uri))) == uri
-    assert parse_query(parse_uri(uri)) == dict()
-
-    uri = abspath
-    assert parse_uri(uri).geturl() == "file:///abspath/file.h5"
     assert str(path_from_uri(parse_uri(uri))) == uri
     assert parse_query(parse_uri(uri)) == dict()
 
@@ -37,6 +35,46 @@ def test_file_uri():
     assert parse_uri(uri).geturl() == "file:///relpath/file.h5?path=/entry"
     assert str(path_from_uri(parse_uri(uri))) == relpath
     assert parse_query(parse_uri(uri)) == {"path": "/entry"}
+
+
+@pytest.mark.skipif(
+    sys.version_info < (3, 12, 5), reason="Behaviour changes in python3.12.5"
+)
+def test_relpath_file_uri():
+    nonpath = str(Path("file.h5"))
+    relpath = str(Path("relpath") / "file.h5")
+    assert not os.path.isabs(nonpath)
+    assert not os.path.isabs(relpath)
+
+    uri = nonpath
+    assert parse_uri(uri).geturl() == "file:file.h5"
+    assert str(path_from_uri(parse_uri(uri))) == uri
+    assert parse_query(parse_uri(uri)) == dict()
+
+    uri = relpath
+    assert parse_uri(uri).geturl() == "file:relpath/file.h5"
+    assert str(path_from_uri(parse_uri(uri))) == uri
+    assert parse_query(parse_uri(uri)) == dict()
+
+    uri = nonpath + "::/entry"
+    assert parse_uri(uri).geturl() == "file:file.h5?path=/entry"
+    assert str(path_from_uri(parse_uri(uri))) == nonpath
+    assert parse_query(parse_uri(uri)) == {"path": "/entry"}
+
+    uri = relpath + "::/entry"
+    assert parse_uri(uri).geturl() == "file:relpath/file.h5?path=/entry"
+    assert str(path_from_uri(parse_uri(uri))) == relpath
+    assert parse_query(parse_uri(uri)) == {"path": "/entry"}
+
+
+def test_abspath_uri():
+    abspath = str(Path(os.path.sep) / "abspath" / "file.h5")
+    assert os.path.isabs(abspath)
+
+    uri = abspath
+    assert parse_uri(uri).geturl() == "file:///abspath/file.h5"
+    assert str(path_from_uri(parse_uri(uri))) == uri
+    assert parse_query(parse_uri(uri)) == dict()
 
     uri = abspath + "::/entry"
     assert parse_uri(uri).geturl() == "file:///abspath/file.h5?path=/entry"
