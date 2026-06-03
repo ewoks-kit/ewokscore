56fe2e274d2708d80187efe4154442994bfeef30
edgar1993a <edgar.gutierrez-fernandez@esrf.fr>
2026-06-03 13:23:22 +0200

    add test for variables names typos

--- src/ewokscore/tests/test_sequencematcher.py
+++ src/ewokscore/tests/test_sequencematcher.py
@@ -3,7 +3,7 @@ import pytest
 
 
 def test_sequence_matcher_missing_required_inputs():
-    with pytest.raises(ValueError, match="Could this be a typo?"):
+    with pytest.raises(ValueError, match="you got 'valufe'. Could this be a typo?"):
         _ = execute_graph(
             graph={
                 "graph": {"id": "test"},
@@ -23,3 +23,61 @@ def test_sequence_matcher_missing_required_inputs():
                 },
             ],
         )
+
+
+def test_sequence_matcher_typo_required_inputs():
+    with pytest.raises(
+        ValueError,
+        match="The required key 'list' is missing and you provide 'listx'. Could this be a typo?",
+    ):
+        _ = execute_graph(
+            graph={
+                "graph": {"id": "test"},
+                "nodes": [
+                    {
+                        "id": "sumlist1",
+                        "task_type": "class",
+                        "task_identifier": "ewokscore.tests.examples.tasks.sumlist.SumList",
+                    }
+                ],
+            },
+            inputs=[
+                {
+                    "id": "sumlist1",
+                    "name": "listx",
+                    "value": [1, 2, 3],
+                },
+            ],
+        )
+
+
+def test_sequence_matcher_typo_optional_inputs(caplog):
+    with caplog.at_level("WARNING", logger="ewokscore.task"):
+        _ = execute_graph(
+            graph={
+                "graph": {"id": "test"},
+                "nodes": [
+                    {
+                        "id": "sumlist1",
+                        "task_type": "class",
+                        "task_identifier": "ewokscore.tests.examples.tasks.sumlist.SumList",
+                    }
+                ],
+            },
+            inputs=[
+                {
+                    "id": "sumlist1",
+                    "name": "list",
+                    "value": [1, 2, 3],
+                },
+                {
+                    "id": "sumlist1",
+                    "name": "delayx",
+                    "value": 1,
+                },
+            ],
+        )
+    assert (
+        "The optional key 'delay' is missing and you provide 'delayx'. Could this be a typo?"
+        in caplog.text
+    )
